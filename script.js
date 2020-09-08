5ae18f4edd5c92626982e8c116d67484c45706d4
MichelFonseca1024 <michelfonseca.nic123@gmail.com>
2020-09-08 00:02:50 -0300

    scrools and animations

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// scrool dynamic
 
+const menuItens = document.querySelectorAll('.menu a[href^="#"]');
 
+menuItens.forEach((itenMenu) => {
+  itenMenu.addEventListener("click", scrollToById);
+});
 
+function scrollToById(event) {
+  event.preventDefault();
+  const element = event.target;
+  const to = getScroolTopByHref(element) - 200;
+  scroolToPosition(to);
+}
+
+function scroolToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+}
+
+function getScroolTopByHref(event) {
+  const id = event.getAttribute("href");
+  const to = document.querySelector(id).offsetTop;
+  return to;
+}
+
+//efects in elements
+
+const target = document.querySelectorAll("[data-anime]");
+
+const animeteClass = "animate";
+
+function animeScrool() {
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  target.forEach((element) => {
+    console.log(element.offsetTop);
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animeteClass);
+    } else {
+      element.classList.remove(animeteClass);
+    }
+  });
+}
+
+if (target.length) {
+  window.addEventListener("scroll", () => {
+    animeScrool();
+  });
+}
